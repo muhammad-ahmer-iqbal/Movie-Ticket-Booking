0f828119ecb13bf006689260999fc5d80a21dd98
Muhammad Ahmer Iqbal <67710351+muhammad-ahmer-iqbal@users.noreply.github.com>
2021-09-21 02:44:02 +0500

    forms updated

---
+++ forms/class-create.php
@@ -0,0 +1,19 @@
+<?php
+
+if(isset($_POST['submit']))
+    {
+        $conn = mysqli_connect('localhost', 'root', '', 'movie_booking_system');
+
+        // $id = $_POST['class_id'];
+        $name = $_POST['class_name'];
+        $price = $_POST['class_price'];
+
+        $query = "INSERT INTO 'class' VALUES('', '$name', '$price')";
+
+        $ins = mysqli_query($conn, $query);
+
+        mysqli_close($conn);
+
+        header("location:class-index.php?message=Added Successfully");
+    }
+?>
