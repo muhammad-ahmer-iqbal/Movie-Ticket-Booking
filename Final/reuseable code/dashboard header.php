0681f0dc7ff47d4f1c71bcdcbccc7c4dfd8ada29
Muhammad Ahmer Iqbal <67710351+muhammad-ahmer-iqbal@users.noreply.github.com>
2021-09-27 23:54:50 +0500

    Updated

--- Final/reuseable code/dashboard header.php
+++ Final/reuseable code/dashboard header.php
@@ -2,9 +2,8 @@
         <div class="text w-100 py-0">
             <div class="row">
                 <div class="col-3 align-self-center">
-                    <div class="text py-0">
+                    <div class="text mt-4">
                         <h6 class="text-light"><span id="date"></span> <span id="time"></span></h6>
-                        <!-- <h5 class="text-light">Date <span>date</span></h5> -->
                     </div>
                 </div>
                 <div class="col-3 align-self-center">
@@ -38,35 +37,3 @@
             </div>
         </div>
     </section>
-<!-- 
-<section class="home-section">
-    <div class="text w-100 py-0">
-        <div class="row">
-            <div class="col-4 mt-3">
-                <div class="text">
-                    <h6 class="text-light"><span id="date"></span> <span id="time"></span></h6>
-                </div>
-            </div>
-            <div class="col-4 mt-3">
-                <div class="text py-0">
-                    <h1 class="text-center text-light">THEATER.COM</h1>
-                </div>
-            </div>
-            <div class="col-4 mt-4">
-                <div class="text-end">
-                    <div class="dropdown">
-                        <button class="btn text-light dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
-                            <img src="../assets/user.png" alt="profile">
-                            <?php 
-                                echo 'Hello '. $_SESSION['Admin'];
-                            ?>
-                        </button>
-                        <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1" style="overflow: hidden;">
-                            <li><a class="dropdown-item" href="logout.php?logoutAdmin">Logout</a></li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</section> -->
